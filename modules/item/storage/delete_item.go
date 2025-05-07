8a558e6d1e2f6332f4e132ca3e50803dcc73c2b4
vyct1 <thienvy95@gmail.com>
2025-05-07 08:43:41 +0700

    fix bug deletedStatus

--- modules/item/storage/delete_item.go
+++ modules/item/storage/delete_item.go
@@ -6,9 +6,11 @@ import (
 )
 
 func (s *sqlStorage) DeleteItem(ctx context.Context, cond map[string]interface{}) error {
+	deletedStatus := model.ItemStatusDeleted
+
 	if err := s.db.Table(model.TodoItem{}.
 		TableName()).Where(cond).
-		Updates(map[string]interface{}{"status": model.ItemStatusDeleted}).Error; err != nil {
+		Updates(map[string]interface{}{"status": deletedStatus.String()}).Error; err != nil {
 		return err
 	}
 
